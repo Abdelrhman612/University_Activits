d30979be61272478ffddfe2ff8e658121e763ebd
Abdelrhman <abdo612200320@gmail.com>
2025-03-06 07:40:40 +0200

    delet-empty-files

--- src/utills/asyncHandler.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const asyncHandler = (
-  fn: (req: Request, res: Response, next: NextFunction) => Promise<any>
-) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    fn(req, res, next).catch(next); // ⬅️ أي خطأ يتم إرساله تلقائيًا إلى `errorHandler`
-  };
-};
