c6c6296d765f0086aa6c3026c82d4044d54beafe
Abdelrhman <abdo612200320@gmail.com>
2025-03-09 08:47:49 +0200

    Add curd opration

--- src/Controllers/activitiesController.ts
+++ src/Controllers/activitiesController.ts
@@ -7,13 +7,49 @@ const prisma = new PrismaClient();
 export const CreateActivitys = asyncWrapper(
   async (req: Request, res: Response, next: NextFunction) => {
     const { name, description, status, userId } = req.body;
-    const newActivity = await prisma.activity.create({
-      data: { name, description, status, userId },
-    });
     if (!name) {
       const error = new AppError("Name Is Required", 400, fail);
       return next(error);
     }
+    const newActivity = await prisma.activity.create({
+      data: { name, description, status, userId },
+    });
     res.status(201).json({ success: success, date: newActivity });
+  }
+);
+export const GetActivities = asyncWrapper(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const Activitis = await prisma.activity.findMany();
+    res.status(200).json({ status: success, data: Activitis });
+  }
+);
+export const UpdateActivity = asyncWrapper(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { Id } = req.params;
+    const { name, description, status, userId } = req.body;
+    const UpActivity = await prisma.activity.findUnique({
+      where: { id: Id },
+    });
+    if (!UpActivity) {
+      const error = new AppError("Activity Is Not Found", 404, fail);
+      return next(error);
+    }
+    const newActivity = await prisma.activity.update({
+      where: { id: Id },
+      data: { name, description, status, userId },
+    });
+    res.status(200).json({ status: success, data: newActivity });
+  }
+);
+export const deleteActivity = asyncWrapper(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { Id } = req.params;
+    await prisma.activity.delete({ where: { id: Id } });
+
+    res.status(200).json({
+      status: success,
+      message: "Delete Activity is successfully",
+      data: null,
+    });
   }
 );
