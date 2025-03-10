809c9a96744ad6f9e9b9b00e1c82020605511c1b
Abdelrhman <abdo612200320@gmail.com>
2025-03-10 06:26:04 +0200

    Add interface for Activity

--- src/Schema/interfaceUsers.ts
+++ src/Schema/interfaceUsers.ts
@@ -6,3 +6,9 @@ export interface User {
   password?: string;
   role?: string;
 }
+export interface Activity {
+  name?: string;
+  description?: string | null;
+  status?: string;
+  userId?: string | null;
+}
