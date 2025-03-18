4c7d76bbaa54f92ccd4af80ad01165af36bd7709
Abdelrhman <abdo612200320@gmail.com>
2025-03-18 07:36:00 +0200

    Chore: applied new Prisma migrations and updated database schema

---
+++ prisma/migrations/20250316012621_init/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `passwordResetCode` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "passwordResetCode",
+ADD COLUMN     "PasswordResetCode" TEXT;
